e4a5b579ea8a62e11a227719df8fd236470cc11c
Artyom Kurpas <minicoal@gmail.com>
2025-02-25 13:28:31 +1000

    feat: add new icons

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,5 +1,13 @@
-export { default as BrandArrow } from "./brand_arrow.svg"
+export { default as ArrowDownIcon } from "./arrow_down.svg"
+
+export { default as BrandArrowIcon } from "./brand_arrow.svg"
+
+export { default as EyeIcon } from "./eye.svg"
+
+export { default as GearIcon } from "./gear.svg"
 
 export { default as LogoShort } from "./logo_short.svg"
 export { default as LogoLong } from "./logo_long.svg"
 export { default as LogoLongWithoutArrow } from "./logo_long_without_arrow.svg"
+
+export { default as MenuIcon } from "./menu.svg"
