9ed7b70f1e0d3319ceacbca00fd041a69cfbd227
Artyom Kurpas <minicoal@gmail.com>
2025-02-26 23:59:32 +1000

    chore: add new icons

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -6,8 +6,14 @@ export { default as EyeIcon } from "./eye.svg"
 
 export { default as GearIcon } from "./gear.svg"
 
+export { default as HomeIcon } from "./home.svg"
+
 export { default as LogoShort } from "./logo_short.svg"
 export { default as LogoLong } from "./logo_long.svg"
 export { default as LogoLongWithoutArrow } from "./logo_long_without_arrow.svg"
+
+export { default as RoundedArrowsIcon } from "./rounded_arrows.svg"
+
+export { default as RefreshIcon } from "./refresh_icon.svg"
 
 export { default as MenuIcon } from "./menu.svg"
