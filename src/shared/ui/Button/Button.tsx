4b84dc85e0bb49dd7be59e1c1da4ad3e7b111339
Artyom Kurpas <minicoal@gmail.com>
2025-02-27 21:01:04 +1000

    feature: button

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,9 +1,17 @@
 import { FC, HTMLAttributes } from "react"
+import clsx from "clsx"
+import styles from "./Button.module.scss"
 
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  variant: "default"
+  variant: "default" | "contained" | "outlined"
 }
 
-export const Button: FC<ButtonProps> = ({ variant }) => {
-  return <button></button>
+export const Button: FC<ButtonProps> = ({ variant, className, ...props }) => {
+  return (
+    <button
+      className={clsx(styles.button, className)}
+      data-variant={variant}
+      {...props}
+    />
+  )
 }
